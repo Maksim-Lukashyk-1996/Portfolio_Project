0ef0d059f45d97e0400d5c30ff165a3b07a9b4d1
Maksim <stik9935@gmail.com>
2022-05-20 13:18:45 +0300

    Django project

--- Maksim_2022/blog/models.py
+++ Maksim_2022/blog/models.py
@@ -1,18 +1,60 @@
 from django.db import models
 
-# Create your models here.
+# Для блога создали модели
+# Для модели Category: Нужен только:
+# CharField- в котором будет храниться название категории
 class Category(models.Model):
     name = models.CharField(max_length=30)
 
+# ПОЛЯ: title и body-Это те же типы полей, которые использовались в APP: project
 class Post(models.Model):
+
+    # Также нужен: CharField-для ЗАГОЛОВКА
     title = models.CharField(max_length=250)
+
+    # Тело, т.е (body), должно быть длинным.
+    # Поэтому мы используем: TextField()
     body = models.TextField()
+
+    # Поля: created_on и last_modified, хранят объект:
+    # DateTimeField-Содержащий дату и время, когда публикация была создана и изменена соответственно.
+    # В строке 20: DateTimeField принимает аргумент: auto_now_add = True.
+    # Он назначает текущую дату и время этому полю всякий раз, когда создаётся экземпляр этого класса.
     created_on = models.DateTimeField(auto_now_add=True)
+
+    # В данной строчке аргумент: auto_now=True -
+    # Назначает текущую дату и время этому полю всякий раз, когда экземпляр этого класса сохраняется.
+    # Это означает, что когда вы редактируете экземпляр этого класса, date_modified обновляется.
     last_modified = models.DateTimeField(auto_now=True)
+
+    # В данной строчке, происходит связка модели для категорий и сообщений, таким образом,
+    # чтобы многие категории могли быть назначены многим сообщениям.
+    # С помощью типа поля: ManyToManyField, поле связывает модели: Category и Post,
+    # тем самым, позволяет создать связь между двумя таблицами
     categories = models.ManyToManyField('Category', related_name='posts')
 
+# Последняя модель: Comment.
 class Comment(models.Model):
+
+    # Поле: author добавляет имя.
     author = models.CharField(max_length=70)
+
+    # body- тело комментария.
     body = models.TextField()
+
+    # Поле created_on, идентичное полю created_on в Post модели.
     created_on = models.DateTimeField(auto_now_add=True)
+
+    # В данной строчке используется-реляционное поле ForeignKey.
+    # Оно похоже на ManyToManyField, но вместо этого определяет отношение многие к одному.
+    # Это объясняется тем, что одному сообщению можно назначить множество комментариев.
+    # Но нельзя иметь комментарий, который соответствует многим постам.
+    # Поле ForeignKey принимает два аргумента.
+    # Первый аргумент – это другая модель в отношениях, в данном случае Post.
+    # Второй аргумент: говорит Django, что делать, когда сообщение удалено.
+    # Если сообщение удалено, то мы не хотим, чтобы комментарии, связанные с ним, торчали.
+    # Поэтому мы хотим также удалить их, поэтому добавим аргумент on_delete=models.CASCADE.
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
+
+# После того как модели созданы, создаём файлы миграции с помощью:
+# python manage.py makemigrations blog; python manage.py migrate
--- Maksim_2022/blog/views.py
+++ Maksim_2022/blog/views.py
@@ -1,8 +1,7 @@
 from django.shortcuts import render
 from .models import Post, Comment
+from .forms import CommentForm
 
-
-# Create your views here.
 def blog_index(request):
     posts = Post.objects.all().order_by('-created_on')
     context = {
@@ -12,12 +11,40 @@ def blog_index(request):
 
 def blog_detail(request, pk):
     post = Post.objects.get(pk=pk)
+    # Создал экземпляр формы и записал в: form
+    form = CommentForm()
+    # затем мы проверяем, если в request, который пришёл к нам со страницы, есть метод: 'POST'
+    if request.method == 'POST':
+        # Тогда мы создаём экземпляр нашего класса, с данными которые пришли из: request.POST
+        form = CommentForm(request.POST)
+        # Проверил или наша форма валидна, всё хорошо и ошибок нет
+        if form. is_valid():
+            # Тогда создаём экземпляр нашего класса: comment в базу данных, чтобы записать туда данные
+            comment = Comment(
+                author=form.cleaned_data['author'],
+                body=form.cleaned_data['body'],
+                post=post
+            )
+            # Сохраняем нашу форму
+            comment.save()
     comments = Comment.objects.filter(post=post)
     context = {
         "post": post,
         "comments": comments,
+        "form": form
     }
 
     return render(request, "blog_detail.html", context)
+
+
+def blog_category(request,category):
+    post = Post.objects.filter(
+        category__name__contains=category
+    ).order_by('-created_on')
+    context = {
+        'post': post,
+        'category': category
+    }
+    return render(request, "blog_category.html", context)
 
 
